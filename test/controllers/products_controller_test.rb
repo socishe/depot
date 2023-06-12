49139e23a24efe4d2aae7daf23f1048376ec11a5
Sihle Socishe <sihlesocishe@gmail.com>
2023-06-12 06:18:53 +0200

    Iteration D2: Connecting Products to Carts

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -6,6 +6,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     @title = "The Greate Book #{rand(1000)}"
   end
 
+  test "can't delete product in cart" do assert_difference("Product.count", 0) do
+    delete product_url(products(:two)) end
+      assert_redirected_to products_url
+  end
+  
   test "should get index" do
     get products_url
     assert_response :success
